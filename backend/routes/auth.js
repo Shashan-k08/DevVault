6fcef90a4e54805f3ddf7fff102b220bb4f7b243
Shashank Tiwari <shashanktiwari3509@gmail.com>
2023-05-17 22:18:06 +0530

    mail added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const otpGenerator = require('otp-generator')
 const User = require('../models/User');
-const {Otp}=require('../models/Otp');
+const { Otp } = require('../models/Otp');
 const router = express.Router();
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
@@ -15,11 +15,11 @@ router.post('/createuser', [
   body('password', 'Password must be at least 5 characters').isLength({ min: 5 }),
 ],
   async (req, res) => {
-    let success=false;
+    let success = false;
     // if there are errors then check error and rwturn bad request and that error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({success, errors: errors.array() });
+      return res.status(400).json({ success, errors: errors.array() });
     }
     // check weather user with this email already exist
     try {
@@ -49,8 +49,35 @@ router.post('/createuser', [
       // .then(user => res.json(user))
       // .catch(err=>{console.log(err)
       // res.json({error:'Please enter a unique value for email'})})
-   success=true;
-      res.json({success,verificationtoken });
+      success = true;
+      res.json({ success, verificationtoken });
+      async (name, email) => {
+        var transporter = nodemailer.createTransport(smtpTransport({
+          service: 'gmail',
+          host: 'smtp.gmail.com',
+          auth: {
+            user: 'shashank2110038@akgec.ac.in',
+            pass: 'mvqgdfcfnmkfqlay'
+            //pass: 'opwkndyvjzvwadhc'
+          }
+        }));
+
+        var mailOptions = {
+          from: 'Shashank',
+          to: email,
+          subject: "iNotebook",
+          html: `Hello <b>${name}</b>, <br>Thank you for Joining Us <br/>We are here to provide best service to you <br/> <br/> <br/><b>Team iNotebook </b>`
+        };
+
+        await transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+        });
+        // res.status(200).json({msg:"asdf"}) 
+      }
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal server error");
@@ -62,7 +89,7 @@ router.post('/login', [
   body('password', 'Password cannot be blank').exists(),
 ],
   async (req, res) => {
-    let success=false;
+    let success = false;
     // if there are errors then check error and rwturn bad request and that error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -75,14 +102,14 @@ router.post('/login', [
 
       let user = await User.findOne({ email });
       if (!user) {
-        success=false
+        success = false
         return res.status(400).json({ error: "Please try login with correct crendentials" });
       }
 
       const passwordcheck = await bcrypt.compare(password, user.password);
       if (!passwordcheck) {
-        success=false
-        return res.status(400).json({success, error: "Please try login with correct crendentials" });
+        success = false
+        return res.status(400).json({ success, error: "Please try login with correct crendentials" });
 
       }
 
@@ -92,8 +119,35 @@ router.post('/login', [
         }
       }
       const verificationtoken = jwt.sign(data, JWT_SECRET);
-      success=true;
-      res.json({success, verificationtoken });
+      success = true;
+      res.json({ success, verificationtoken });
+      const temp = async(name,email)=>{
+        var transporter = nodemailer.createTransport(smtpTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        auth: {
+            user: 'shashank2110038@akgec.ac.in',
+            pass: 'mvqgdfcfnmkfqlay'
+            //pass: 'opwkndyvjzvwadhc'
+        }
+        }));
+    
+        var mailOptions = {
+        from: 'Shashank',
+        to: email, 
+        subject: "iNotebook",
+        html: `Hello <b>${name}</b>, <br> A new device has logged in into your account <br/> <br/> <br/> Thanks <br/><b>Team iNotebook </b>`
+        };
+    
+       await transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+        }); 
+       // res.status(200).json({msg:"asdf"}) 
+    }
 
     } catch (error) {
       console.error(error.message);
@@ -101,18 +155,18 @@ router.post('/login', [
     }
   })
 
-  // Rout :3 Get logged in User details using :Post "/api/auth/getuser".Login Required
+// Rout :3 Get logged in User details using :Post "/api/auth/getuser".Login Required
 
 router.post('/getuser', fetchuser, async (req, res) => {
-   try {
-    userId =req.user.id;
+  try {
+    userId = req.user.id;
     const user = await User.findById(userId).select("-password")
     res.send(user)
-    
-   } catch (error) {
+
+  } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal server error");
-   }
+  }
 
-  })
+})
 module.exports = router
